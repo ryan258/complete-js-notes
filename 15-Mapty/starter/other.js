baa6a84ba6cda3c3612048d3868fd042e52f8a41
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-02-24 07:41:16 -0600

    Refactor to OOP architecture.

---
+++ 15-Mapty/starter/other.js
@@ -0,0 +1,2 @@
+const firstName = 'Ryan';
+console.log(months);
--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,77 +11,100 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map;
-let mapEvent;
+class App {
+  #map;
+  #mapEvent;
+  // this constructor method is called immediately when a new instance is created from this class
+  constructor() {
+    // so we can just keep things tidy by getting the position in the constructor
+    this._getPosition();
+    // we can also attach our event handlers to the new instance in the constructor too!
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // position object automatically comes from .getCurrentPosition()
-      // console.log(position);
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      // console.log(`Lat: ${latitude}\nLong: ${longitude}`);
-      // console.log(`https://www.google.com/maps/@${latitude},${longitude}z`);
-
-      const coords = [latitude, longitude];
-
-      // L.map()
-      // L = main leaflet entry point
-      console.log(L);
-      // - .map(xxx) = xxx = id of element you want to put the map
-      // const map = L.map('map').setView([51.505, -0.09], 13);
-      map = L.map('map').setView(coords, 13);
-      console.log(map);
-      // the map is made up of small tiles - from open street map
-      // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      // this .on() is coming from the leaflet library
-      //! Handle clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert('Could not get your position.');
+  _getPosition() {
+    if (navigator.geolocation) {
+      // JS will callback _loadmap as soon as the _loadMap's position is identified
+      //! bind to give this._loadMap definition
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this), //! bind this to the current object
+        function () {
+          alert('Could not get your position.');
+        }
+      );
     }
-  );
+  }
+
+  _loadMap(position) {
+    // position object automatically comes from .getCurrentPosition()
+    // console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // console.log(`Lat: ${latitude}\nLong: ${longitude}`);
+    // console.log(`https://www.google.com/maps/@${latitude},${longitude}z`);
+
+    const coords = [latitude, longitude];
+
+    console.log(L);
+    // const map = L.map('map').setView([51.505, -0.09], 13);
+    // Error: ... cannot set property '#map' of undefined
+    // - something must be wrong with the 'this' keyword
+    console.log(this); // undefined, because _loadMap() is being called by _getPosition as .getCurrentPosition's callback function and is called as a regular function call, which sets 'this' to undefined.
+    this.#map = L.map('map').setView(coords, 13);
+    // console.log(map);
+    // the map is made up of small tiles - from open street map
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // this .on() is coming from the leaflet library
+    //! Handle clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    // submitting a form for a new workout will create a new workout
+    e.preventDefault();
+    // console.log(this);
+
+    // Clear Input Fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+      '';
+
+    // Display Marker
+    // console.log(this.#mapEvent);
+    const { lat, lng } = this.#mapEvent.latlng;
+    // L.marker([51.5, -0.09])
+    // L.marker([lat, lng]).addTo(map).bindPopup('👻 👻 👻').openPopup();
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('👻🔥 EXERCISE 🔥👻')
+      .openPopup();
+  }
 }
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // Clear Input Fields
-  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
-    '';
-
-  // Display Marker
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-  // L.marker([51.5, -0.09])
-  // L.marker([lat, lng]).addTo(map).bindPopup('👻 👻 👻').openPopup();
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('👻🔥 EXERCISE 🔥👻')
-    .openPopup();
-});
-
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+// create an instance of our app class
+const app = new App();
